2e151496b55c6b3a30efd0e13ab2451666d8a710
Ed Jefferson <e.jefferson@gmail.com>
2013-10-02 12:48:35 +0100

    user model in place

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001151201) do
+ActiveRecord::Schema.define(version: 20131002114354) do
 
   create_table "games", force: true do |t|
     t.string   "player_one"
@@ -21,5 +21,14 @@ ActiveRecord::Schema.define(version: 20131001151201) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
 
 end
