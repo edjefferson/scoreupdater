1a1a00a0e1eb259ac6fb3c99d27cb1ce0ea7e769
Ed Jefferson <e.jefferson@gmail.com>
2013-10-11 12:06:10 +0100

    player number can be nil

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ActiveRecord::Base
-  validates :player_no, inclusion: { in: [1,2],
+  validates :player_no, inclusion: { in: [1,2, nil],
     message: "%{value} is not a valid player number" }
 end
